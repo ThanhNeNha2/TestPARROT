3da1abc199cb084ea37584e2588a25ee0034a5c3
mtriet100505 <ta.coaching.v2@gmail.com>
2025-08-30 01:00:05 +0700

    update library configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useEffect } from 'react';
-
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-400">
-        Võ Chí Thanh
+        Võ Chí Thanh ádasd
       </h1>
     </>
   );
