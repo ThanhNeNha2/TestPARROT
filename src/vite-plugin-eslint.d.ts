9274825796573138ac6789ae5cf4970b9415efb7
mtriet100505 <ta.coaching.v2@gmail.com>
2025-08-30 22:26:59 +0700

    deploy hosting free

--- src/vite-plugin-eslint.d.ts
+++ src/vite-plugin-eslint.d.ts
@@ -1,1 +1,5 @@
-declare module 'vite-plugin-eslint';
+declare module 'vite-plugin-eslint' {
+  import { Plugin } from 'vite';
+  function eslintPlugin(options?: Record<string, unknown>): Plugin;
+  export default eslintPlugin;
+}
